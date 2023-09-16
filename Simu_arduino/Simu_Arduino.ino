77a8f9ace85eace9a91d3c29f2e663a779f0df37
Leonardo Melo <leonardomelo402@gmail.com>
2023-09-16 18:47:19 -0300

    otimização da racepção na ACC_ECU e alteração da método de envio da Simu_arduino, agora essa ECU envia apenas uma mensagem com todos os dados

--- Simu_arduino/Simu_Arduino.ino
+++ Simu_arduino/Simu_Arduino.ino
@@ -1,7 +1,19 @@
 #include <stdbool.h>
 #include "Arduino.h"
 #include "mcp_can.h"
-//#include "tpl_os.h"
+
+
+#define EV_RV_RD_data_ID    0x0C1B3049 
+#define Ego_Acceleration_ID	0x0D4001B0
+
+
+#define DLC			8
+#define EXT_FRAME 	1
+
+
+#define BUFF_MAX 	10
+#define BUFF_MIN 	00
+volatile int		buffer = BUFF_MAX;
 
 
 float Ego_ace       = 0;
@@ -9,41 +21,35 @@ float Lead_ace      = 0;
 float Relative_velo = 0;
 float Lead_pos      = 50.0;                            //Initial lead car position (m)
 float Ego_pos       = 10.0;                            //Initial ego car position  (m)
+float Lead_velo 	= 90.0/3.6;                        //Initial lead car position (m/s)
+float Ego_velo  	= 70.0/3.6;                        //Initial ego car position  (m/s)
+float interval  	= 0.001;
+int   counter   	= 0;
 float Relative_distance_pres = 10;
 float Relative_distance_past = 5;
-float Lead_velo = 90.0/3.6;                            //Initial lead car position (m/s)
-float Ego_velo  = 70.0/3.6;                            //Initial ego car position  (m/s)
-float interval  = 0.001;
-int   counter   = 0;
-char Relative_distance_pres_send[4];
-char Ego_Speed_send[4];
-char Relative_Speed_send[4];
+
+char  Relative_distance_pres_send[4];
+char  Ego_Speed_send[4];
+char  Relative_Speed_send[4];
 
 
-#define Ego_Speed_ID			0x0C43A1B4
-#define Relative_Distance_ID	0x0A1B8008 
-#define Ego_Acceleration_ID		0x0D4001B0
-#define Relative_Speed_ID		0x0C1B3049 
+unsigned char Send_data[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
-#define DLC			8
-#define EXT_FRAME 	1
 
-unsigned char Ego_Speed_data[8]         = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-unsigned char Relative_Distance_data[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-unsigned char mSim3_data[8] 			= {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-unsigned char Relative_Speed_data[8]    = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 unsigned char mDATA[8];
-unsigned char mDLC = 0;
+unsigned char mDLC    = 0;
 long unsigned int mID = 0;
 byte ret = 0;
 
+
 MCP_CAN CAN1(10);
 
-void setup() {
-	//serial: baudrate = 115200
 
-  float Relative_distance_pres = Lead_pos - Ego_pos;     //Relative distance between Ego and Lead Car.
-  float Relative_distance_past = Relative_distance_pres; //Past value of relative distance.
+void setup() 
+{
+	float Relative_distance_pres = Lead_pos - Ego_pos;     //Relative distance between Ego and Lead Car.
+	float Relative_distance_past = Relative_distance_pres; //Past value of relative distance.
+	
 	Serial.begin(115200);
 	
 	//Start CAN controller : baudrate = 250K, clock=8MHz
@@ -54,19 +60,19 @@ void setup() {
 	//Defines operation mode
 	CAN1.setMode(MCP_NORMAL);
 	pinMode(2, INPUT);
+	
 }
 
 void loop()
 {
-	
 	if(!digitalRead(2)){
-	CAN1.readMsgBuf(&mID, &mDLC, mDATA);
-	if((mID & Ego_Acceleration_ID) == Ego_Acceleration_ID){
-		Ego_ace = (mDATA[1] << 8) | mDATA[2];
-    Ego_ace = (Ego_ace-5)*0.01;
-    Serial.println("RECEBEU ACC");
+		CAN1.readMsgBuf(&mID, &mDLC, mDATA);
+		if((mID & Ego_Acceleration_ID) == Ego_Acceleration_ID){
+			Ego_ace = (mDATA[1] << 8) | mDATA[2];
+			Ego_ace = (Ego_ace-5)*0.01;
+			Serial.println("RECEBEU ACC");
 		}else{
-		Ego_ace = Ego_ace;
+			Ego_ace = Ego_ace;
 		}
 	}
 	
@@ -82,8 +88,6 @@ void loop()
 	//Calculation of Lead Car position
 	Lead_pos += (Lead_velo*interval);
 
-	//counter++;
-
 	Relative_distance_pres = Lead_pos - Ego_pos;
 	Relative_velo = Lead_velo - Ego_velo;
 
@@ -95,64 +99,38 @@ void loop()
 		
 	counter++;
 	
-		if (counter % 20000 == 0){
-			Serial.print("Ego_ace: ");
-			Serial.print(Ego_ace);
-			Serial.print("; ");
+	if (counter % 20000 == 0){
 		
-			Serial.print("Relative Distance: ");
-			Serial.print(Relative_distance_pres);
-			Serial.print("; ");
-		
-		
-			Serial.print("EGO SPEED: ");
-			Serial.print(Ego_velo);
-			Serial.print("; ");
+		Serial.print("Rebendo ---- Ego_ace: ");
+		Serial.print(Ego_ace);
+		Serial.println("; ");
 	
+		Serial.print("Enviando ---- Relative Distance: ");
+		Serial.print(Relative_distance_pres);
+		Serial.print("; ");
+		
+		Serial.print("EGO SPEED: ");
+		Serial.print(Ego_velo);
+		Serial.print("; ");
 
-			Serial.print("Relative Speed: ");
-			Serial.print(Relative_velo);
-			Serial.println("; ");
-			//TerminateTask();
+		Serial.print("Relative Speed: ");
+		Serial.print(Relative_velo);
+		Serial.println("; ");
+		
+		//CAN Message Sender
 	
-
-			//Ego CAN Message Sender
+		sprintf(Ego_Speed_send , "%04X", (int)(Ego_velo*256));
+		Send_data[1] = strtol(Ego_Speed_send , NULL, 16) >> 8;
+		Send_data[2] = strtol(Ego_Speed_send  + 2, NULL, 16);
 	
-			sprintf(Ego_Speed_send , "%04X", (int)(Ego_velo*256));
-			Ego_Speed_data[1] = strtol(Ego_Speed_send , NULL, 16) >> 8;
-			Ego_Speed_data[2] = strtol(Ego_Speed_send  + 2, NULL, 16);
+		sprintf(Relative_distance_pres_send , "%04X", (int)Relative_distance_pres);
+		Send_data[3] = strtol(Relative_distance_pres_send , NULL, 16) >> 8;
+		Send_data[4] = strtol(Relative_distance_pres_send  + 2, NULL, 16);
 		
-      ret = 0;
-			ret = CAN1.sendMsgBuf(Ego_Speed_ID,EXT_FRAME,DLC,Ego_Speed_data);
-      	if (ret == CAN_OK)
-        {
-		      Serial.print("EGO SPEED. Id =");
-          Serial.println("Ego_Speed_ID");
-        }
-
-  
-			//Relative Distance CAN Message Sender
-			//Relative_distance_pres = Relative_distance_pres;
+		sprintf(Relative_Speed_send , "%04X", (int)(Relative_velo*256));
+		Send_data[5] = strtol(Relative_Speed_send , NULL, 16) >> 8;
+		Send_data[6] = strtol(Relative_Speed_send  + 2, NULL, 16);
 	
-			sprintf(Relative_distance_pres_send , "%04X", (int)Relative_distance_pres);
-			Relative_Distance_data[1] = strtol(Relative_distance_pres_send , NULL, 16) >> 8;
-			Relative_Distance_data[2] = strtol(Relative_distance_pres_send  + 2, NULL, 16);
-		
-		
-			ret = CAN1.sendMsgBuf(Relative_Distance_ID,EXT_FRAME,DLC,Relative_Distance_data);
-
-
-			//Relative Speed CAN Message Sender
-		
-		
-			sprintf(Relative_Speed_send , "%04X", (int)(Relative_velo*256));
-			Relative_Speed_data[1] = strtol(Relative_Speed_send , NULL, 16) >> 8;
-			Relative_Speed_data[2] = strtol(Relative_Speed_send  + 2, NULL, 16);
-		
-			ret = CAN1.sendMsgBuf(Relative_Speed_ID,EXT_FRAME,DLC,Relative_Speed_data);
-		
-		}
-//		
-//			exit(0);
-//		
+		ret = CAN1.sendMsgBuf(EV_RV_RD_data_ID,EXT_FRAME,DLC,Send_data);
+	}	
 }
